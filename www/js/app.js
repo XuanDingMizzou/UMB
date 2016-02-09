a2bd4bd80d3b14592acee164d16030197b8f7ed0
xuanding <dxuan120@gmail.com>
2016-02-08 22:27:19 -0600

    Second Skeleton

--- www/js/app.js
+++ www/js/app.js
@@ -110,6 +110,6 @@ angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/login');
+  $urlRouterProvider.otherwise('/tab/account');
 
 });
